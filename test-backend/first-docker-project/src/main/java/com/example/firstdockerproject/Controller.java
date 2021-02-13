9f59e236a84d4b0ca3f05742d88f73d2303bee79
Aleksandar Milicevic <milicoa97@gmail.com>
2021-02-13 18:25:45 +0100

    add validation

---
+++ test-backend/first-docker-project/src/main/java/com/example/firstdockerproject/Controller.java
@@ -0,0 +1,18 @@
+package com.example.firstdockerproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+
+    @GetMapping()
+    public String test() {
+        return "Hello world";
+    }
+
+
+
+}
