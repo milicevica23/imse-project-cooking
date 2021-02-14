c05a177ce491a6ca7fcd2ba843b75458ccc7f518
Aleksandar Milicevic <milicoa97@gmail.com>
2021-02-14 01:02:48 +0100

    docker for second project and docker compose

--- test-backend/first-docker-project/src/main/java/com/example/firstdockerproject/Controller.java
+++ test-backend/first-docker-project/src/main/java/com/example/firstdockerproject/Controller.java
@@ -4,15 +4,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @RestController
 @RequestMapping("/test")
 public class Controller {
 
     @GetMapping()
-    public String test() {
-        return "Hello world";
+    public String test() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .header("accept", "application/json")
+                .uri(URI.create("http://python:5000/helloworld"))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+       // return "hello version 2";
     }
-
-
 
 }
